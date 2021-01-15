deb7d394ef1259dfc6f5b9f9b2a83b9228ef84e0
RastogiTanya <rastogitanya96@gmail.com>
2021-01-15 22:15:39 +0530

    Managing contacts and numbers in python

---
+++ task3/x.py
@@ -0,0 +1,14 @@
+d = {"x" : 1}
+
+print(list(d.keys()))
+# numbers = {
+#     "1": "one",
+#     "2": "two",
+#     "3": "three",
+#     "4": "four"
+# }
+# enter = input('Phone: ')
+# output=""
+# for ch in numbers:
+#     output += numbers.get(ch,"!") + " "
+# print(output)
